7d437e32b74e18e236f2704f996e71a165009989
pclaro <1337pedro1337@gmail.com>
2022-01-26 12:10:53 +0000

    pequenas alterações antes de almoço
    Co-authored-by: edsonCVN <edsonCVN@users.noreply.github.com>

--- tecnicofs_ex2/fs/tfs_server.c
+++ tecnicofs_ex2/fs/tfs_server.c
@@ -1,4 +1,6 @@
 #include "operations.h"
+#include <errno.h>
+#include <fcntl.h>
 
 int main(int argc, char **argv) {
 
@@ -10,7 +12,25 @@ int main(int argc, char **argv) {
     char *pipename = argv[1];
     printf("Starting TecnicoFS server with pipe called %s\n", pipename);
 
-    /* TO DO */
+    if (unlink(pipename) != 0 && errno != ENOENT) {
+        fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", pipename, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // create pipe
+    if (mkfifo(pipename, 0640) != 0) {
+        fprintf(stderr, "[ERR]: mkfifo failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    //open clients->server pipe for reading
+    int rx = open(pipename, O_RDONLY);
+    if (rx == -1) {
+        fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    //while
 
     return 0;
 }
